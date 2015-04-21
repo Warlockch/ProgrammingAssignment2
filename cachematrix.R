af6aa3863abec312da292b8a8fd478e7c3625d1d
Warlock <tshwanglike@gmail.com>
2015-04-22 03:37:28 +0800

    Revert "Update cachematrix.R"
    This reverts commit 462cacd8f16c90d666ec1620f8e29436cfc8e3b3.

--- cachematrix.R
+++ cachematrix.R
@@ -1,31 +1,10 @@
 ## [Put comments here that describe what your functions do]
 
 makeCacheMatrix <- function(x = matrix()) {
-        m <- NULL
-        set <- function(y) {
-                x <<- y
-                m <<- NULL
-        }
-        get <- function() x
-       ## setinverse <- function() m <<- solve(x)
-        getinverse <- function() solve(x)
-        list(set = set, get = get,
-            ## setinverse = setinverse,
-             getinverse = getinverse)
-
 
 }
 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-        m <- x$getinverse()
-        if(!is.null(m)) {
-                message("getting cached data")
-                return(m)
-        }
-        data <- x$get()
-        m <- solve(data, ...)
-        x$setinverse(m)
-        m
 }
